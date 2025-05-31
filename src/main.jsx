f8bbdc78b2ca8ee42aeb534e8caa1b6cc318fcb0
arbaj khan <zaid9982738@gmail.com>
2025-05-31 03:54:56 -0700

    add some text

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,15 @@
 
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import { HashRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import { store } from './Store/Store.jsx'
 
 createRoot(document.getElementById('root')).render(
     <Provider store={store}>
-      <HashRouter>
+      <BrowserRouter>
         <App />
-      </HashRouter>
+      </BrowserRouter>
     </Provider>
 
 )
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/FlixNow-Movie/',
+  base: "/FlixNow-Movie/", 
   plugins: [react()],
+  build: {
+    outDir: 'dist'
+  },
+  
+  server: {
+    historyApiFallback: true
+  }
 })
